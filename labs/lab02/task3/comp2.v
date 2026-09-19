03cb7d58468cdf9da560792f757db5f7c837970c
Ayushkr-coder <f20250708@pilani.bits-pilani.ac.in>
2026-09-19 11:33:33 +0000

    lab02 task3

--- labs/lab02/task3/comp2.v
+++ labs/lab02/task3/comp2.v
@@ -8,6 +8,11 @@
 // Write your testbench first, let it tell you something is wrong, THEN
 // come back and fix this file.
 
+// comp2.v
+// 2-bit unsigned magnitude comparator.
+// Given two 2-bit values A and B, exactly one of GT, LT, EQ should be 1
+// for any input combination.
+
 module comp2 (
   input  [1:0] A,
   input  [1:0] B,
@@ -17,7 +22,7 @@ module comp2 (
 );
 
   assign EQ = (A == B);
-  assign GT = (A >= B);
-  assign LT = (A <  B);
+  assign GT = (A > B);
+  assign LT = (A < B);
 
 endmodule
